7f7b9fcf09e796790a9290f561c7d3a3873568bd
Sebastian Kippe <sebastian@kip.pe>
2016-06-19 15:02:37 +0200

    Go to day in calendar, if previous/next not available in log
    If there's no previous/next date in the daily log archive (which in most
    cases means there's likely no log archive for the day at all), then show
    a button that goes to the previous/next day in the calendar instead.
    Except if the next day is in the future.
    
    refs #4

--- app/controllers/logs.js
+++ app/controllers/logs.js
@@ -16,32 +16,48 @@ export default Ember.Controller.extend({
            [dateParams.year, dateParams.month, dateParams.day].join('/');
   }.property('model.archive.today.[@id]', 'model.dateParams'),
 
+  previousDay: function() {
+    let dateParams = this.get('model.dateParams');
+    let dateStr = [dateParams.year, dateParams.month, dateParams.day].join('-');
+    return moment.utc(dateStr).subtract(1, 'days').format('YYYY/MM/DD');
+  }.property('model.dateParams'),
+
+  nextDay: function() {
+    let dateParams = this.get('model.dateParams');
+    let dateStr = [dateParams.year, dateParams.month, dateParams.day].join('-');
+    let nextDay = moment.utc(dateStr).add(1, 'days');
+
+    if (nextDay.isSameOrAfter(moment.utc())) {
+     return null;
+    } else {
+     return nextDay.format('YYYY/MM/DD');
+    }
+  }.property('model.dateParams'),
+
   previousClass: function() {
-    return Ember.isPresent(this.get('previous')) ? 'enabled' : 'disabled';
-  }.property('previous'),
+    return Ember.isPresent(this.get('previousDay')) ? 'enabled' : 'disabled';
+  }.property('previousDay'),
 
   nextClass: function() {
-    return Ember.isPresent(this.get('next')) ? 'enabled' : 'disabled';
-  }.property('next'),
+    return Ember.isPresent(this.get('nextDay')) ? 'enabled' : 'disabled';
+  }.property('nextDay'),
 
   actions: {
 
     goToPreviousDay() {
-      if (!this.get('previous')) { return; }
-
       let path = '/logs/'+this.get('model.network')+
                  '/'+this.get('model.channel')+
-                 '/'+this.get('previous');
+                 '/'+(this.get('previous') || this.get('previousDay'));
 
       this.transitionToRoute(path);
     },
 
     goToNextDay() {
-      if (!this.get('next')) { return; }
+      if (!this.get('nextDay')) { return; } // next day in future
 
       let path = '/logs/'+this.get('model.network')+
                  '/'+this.get('model.channel')+
-                 '/'+this.get('next');
+                 '/'+(this.get('next') || this.get('nextDay'));
 
       this.transitionToRoute(path);
     }
